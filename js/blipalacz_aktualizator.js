2664dcf7e16eec9a9ebd68a4c25975617a6756a4
Jakub Bogucki <jakub300@gmail.com>
2011-03-15 17:42:54 +0100

    dodanie moich urli w celu przetestowania aktualizacji

--- js/blipalacz_aktualizator.js
+++ js/blipalacz_aktualizator.js
@@ -23,10 +23,12 @@ blipalacz.p.aktualizator.sprawdz = function() {
 
 blipalacz.p.aktualizator.url = {};
 blipalacz.p.aktualizator.url['stable'] = false;
-blipalacz.p.aktualizator.url['nightly'] = 'https://github.com/kubofonista/BLIPalacz/blob/nightly/ver_nightly.txt';
-blipalacz.p.aktualizator.url['new-alpha'] = 'https://github.com/kubofonista/BLIPalacz/blob/new-alpha/ver_nightly.txt';
+blipalacz.p.aktualizator.url['nightly'] = 'https://github.com/kubofonista/BLIPalacz/raw/nightly/ver_nightly.txt';
+//blipalacz.p.aktualizator.url['new-alpha'] = 'https://github.com/kubofonista/BLIPalacz/raw/new-alpha/ver_nightly.txt';
+blipalacz.p.aktualizator.url['new-alpha'] = 'https://github.com/jakub300/BLIPalacz/raw/nightly/ver_nightly.txt';
 
 blipalacz.p.aktualizator.urld = {};
 blipalacz.p.aktualizator.urld['stable'] = 'https://github.com/kubofonista/BLIPalacz/raw/master/blipalacz.user.js';
 blipalacz.p.aktualizator.urld['nightly'] = 'https://github.com/kubofonista/BLIPalacz/raw/nightly/blipalacz.user.js';
-blipalacz.p.aktualizator.urld['new-alpha'] = 'https://github.com/kubofonista/BLIPalacz/raw/new-alpha/blipalacz.user.js';
+blipalacz.p.aktualizator.urld['new-alpha'] = 'https://github.com/jakub300/BLIPalacz/raw/nightly/blipalacz.user.js';
+//blipalacz.p.aktualizator.urld['new-alpha'] = 'https://github.com/kubofonista/BLIPalacz/raw/new-alpha/blipalacz.user.js';
